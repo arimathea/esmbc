b2b6129e95c796107ad401e952c6437de3b63891
Stuart Baker <sdb@stuartdb.com>
2013-03-17 16:17:36 +1300

    Updated setup file to use distribute

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
-"""
-esmbc
-~~~~~
-
-Calculates total ship volume of supplied ships and quantity pairs.
-
-:copyright: (c) 2012 Stuart Baker
-:license: GNU GPL Version 3, see LICENSE
-
-"""
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='esmbc',
       version='0.1',
@@ -19,12 +9,20 @@ setup(name='esmbc',
       author_email='sdb@stuartdb.com',
       url='https://github.com/stuartdb/esmbc',
       platforms='any',
-      py_modules=['esmbc', 'test_esmbc'],
-      data_files=[('data', ['data/ships.json', 'data/test_ships.json'])],
+      packages=['esmbc'],
+      package_data={'esmbc': ['ships.json']},
+      entry_points={
+        'console_scripts': [
+            'esmbc = esmbc.__main__:main',
+            ],
+        },
+      install_requires=['distribute'],
       classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Topic :: Games/Entertainment'
         ]
