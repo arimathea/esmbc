1b9c5c19d47d73b7244d0e37762c4969ce95ca86
Stuart Baker <sdb@stuartdb.com>
2013-03-21 17:23:25 +1300

    Updated tests to expect None values instead of exceptions

--- test/test_esmbc.py
+++ test/test_esmbc.py
@@ -11,7 +11,8 @@ Test file for esmbc
 """
 import unittest
 import os
-import esmbc.cli as esmbc
+from esmbc import cli as esmbc
+
 
 class TestEsmbc(unittest.TestCase):
 
@@ -21,23 +22,27 @@ class TestEsmbc(unittest.TestCase):
         #     "zephyr":5000.0
         # }
         volumes = {'zephyr': 5000.0}
-        esmbc_dir = os.path.dirname(os.path.realpath(__file__))
-        filename = os.path.join(esmbc_dir, 'test_ships.json')
+
+        test_dir = os.path.dirname(os.path.realpath(__file__))
+        filename = os.path.join(test_dir, 'test_ships.json')
 
         self.assertEqual(esmbc.load_volumes(filename), volumes)
+
+        self.assertRaises(FileNotFoundError, esmbc.load_volumes, '')
+        self.assertRaises(FileNotFoundError, esmbc.load_volumes, 42)
         self.assertRaises(FileNotFoundError, esmbc.load_volumes, 'foo.json')
 
     def test_parse_ship_pairs(self):
-        pairs = ['foo:1', 'bar:2']
-        counts = {'foo' : '1', 'bar' : '2'}
+        pairs = ['foo:1', 'BAR:2']
+        expected = {'foo' : '1', 'bar' : '2'}
 
-        self.assertDictEqual(counts, esmbc.parse_ship_pairs(pairs))
-        self.assertRaisesRegex(ValueError,
-                               'Invalid argument format',
-                               esmbc.parse_ship_pairs, 'foo')
-        self.assertRaisesRegex(Exception,
-                               'No ship and count pairs were supplied',
-                               esmbc.parse_ship_pairs, [])
+        self.assertDictEqual(expected,
+                             esmbc.parse_ship_pairs(pairs))
+
+        self.assertIsNone(esmbc.parse_ship_pairs('foo'))
+        self.assertIsNone(esmbc.parse_ship_pairs([]))
+        self.assertIsNone(esmbc.parse_ship_pairs(None))
+        self.assertIsNone(esmbc.parse_ship_pairs(''))
 
     def test_calculate_volume_totals(self):
         volumes = {'zephyr': 5000.0}
@@ -45,17 +50,15 @@ class TestEsmbc(unittest.TestCase):
         totals = {'zephyr' : 10000}
 
         self.assertDictEqual(esmbc.calculate_volume_totals(counts, volumes),
-                              totals)
-        self.assertRaisesRegex(Exception, 'foo is not a valid ship',
-                               esmbc.calculate_volume_totals,
-                               {'foo' : '1'}, volumes)
-        self.assertRaisesRegex(Exception, 'No ship counts were supplied',
-                               esmbc.calculate_volume_totals,
-                               {}, volumes)
-
-
-        self.assertRaises(ValueError, esmbc.calculate_volume_totals,
-                         {'zephyr' : 'bar'}, volumes)
+                             totals)
+        self.assertIsNone(esmbc.calculate_volume_totals({'foo' : '1'},
+                                                        volumes))
+        self.assertIsNone(esmbc.calculate_volume_totals({'foo' : '1'},
+                                                        volumes))
+        self.assertIsNone(esmbc.calculate_volume_totals({'zephyr' : 'bar'},
+                                                        volumes))
+        self.assertIsNone(esmbc.calculate_volume_totals({},
+                                                        volumes))
 
     def test_format_table(self):
         totals = {'zephyr' : 10000}
@@ -63,8 +66,8 @@ class TestEsmbc(unittest.TestCase):
         table = '{0}     10000m3'.format(table)
 
         self.assertEqual(esmbc.format_table(totals), table)
-        self.assertRaisesRegex(Exception, 'No volume totals were supplied',
-                               esmbc.format_table, {})
+        self.assertIsNone(esmbc.format_table({}))
+
 
 if __name__ == "__main__":
     unittest.main()
