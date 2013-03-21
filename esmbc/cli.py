07fc0aa7af96cd526a47094073074ce554231579
Stuart Baker <sdb@stuartdb.com>
2013-03-21 17:24:38 +1300

    Updated cli functino to return None instead of raising exceptions in some cases

--- esmbc/cli.py
+++ esmbc/cli.py
@@ -19,7 +19,7 @@ import argparse
 def load_volumes(filename):
     """Loads the ship volume dictionary from a JSON file and returns it"""
     if not os.path.exists(filename):
-        raise FileNotFoundError('Unable to load {0}'.format(filename))
+        raise FileNotFoundError('Unable to load {}'.format(filename))
 
     with open(filename, 'rt') as esmbc_data:
         return json.load(esmbc_data)
@@ -28,18 +28,16 @@ def load_volumes(filename):
 def parse_ship_pairs(pairs):
     """Parses the ship count pairs and returns a ship count dict"""
     if not pairs:
-        raise Exception('No ship and count pairs were supplied')
+        return None
 
     ship_counts = {}
     for pair in pairs:
         try:
             ship, count = pair.split(':', maxsplit=1)
         except ValueError:
-            raise ValueError('Invalid argument format')
-        except:
-            raise Exception('Unknown error occured while parsing ship pairs')
+            return None
         else:
-            ship_counts[ship] = count
+            ship_counts[str.lower(ship)] = count
 
     return ship_counts
 
@@ -47,19 +45,16 @@ def parse_ship_pairs(pairs):
 def calculate_volume_totals(ship_counts, ship_volumes):
     """Builds and returns a dict of ships and subtotal of their volumes"""
     if not ship_counts:
-        raise Exception('No ship counts were supplied')
+        return None
 
     volume_totals = {}
     for ship, count in ship_counts.items():
         if ship not in ship_volumes:
-            raise Exception('{0} is not a valid ship'.format(ship))
-
+            return None
         try:
             volume_totals[ship] = int(ship_volumes[ship]) * int(count)
         except ValueError:
-            raise ValueError('The supplied volume or count is not a number')
-        except:
-            raise Exception('Unknown error occured while calculating volumes')
+            return None
 
     return volume_totals
 
@@ -70,7 +65,7 @@ def format_table(volume_totals):
     table = ''
 
     if not volume_totals:
-        raise Exception('No volume totals were supplied')
+        return None
 
     for ship, subtotal in volume_totals.items():
         total += subtotal
@@ -93,16 +88,24 @@ def main():
 
     try:
         ship_volumes = load_volumes(filename)
-        ship_counts = parse_ship_pairs(args.ships)
-        volume_totals = calculate_volume_totals(ship_counts, ship_volumes)
-        total = format_table(volume_totals)
     except FileNotFoundError as error:
-        sys.stderr.write('{0}\n'.format(error))
-    except ValueError as error:
-        sys.stderr.write('{0}\n'.format(error))
-    except Exception as error:
-        sys.stderr.write('{0}\n'.format(error))
-    else:
-        print('{0}'.format(total))
-    finally:
+        sys.stderr.write('{}\n'.format(error))
         sys.exit()
+
+    ship_counts = parse_ship_pairs(args.ships)
+    if not ship_counts:
+        parser.print_help()
+        sys.exit()
+
+    volume_totals = calculate_volume_totals(ship_counts, ship_volumes)
+    if not volume_totals:
+        parser.print_help()
+        sys.exit()
+
+    total = format_table(volume_totals)
+    if not total:
+        parser.print_help()
+        sys.exit()
+
+    print('{}'.format(total))
+    sys.exit()
